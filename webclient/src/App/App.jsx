32acf4f05779ff2efe83149d852bf3131d9c73d7
Summit <JavaDog.Dev@gmail.com>
2018-03-02 04:28:56 -0500

    Initial Commit
    Basic React + SCSS setup with webpack hot reloading.

---
+++ webclient/src/App/App.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { hot } from 'react-hot-loader';
+
+import './App.scss';
+
+const App = () => (
+  <div className="wrapper">
+    Hello from React!
+  </div>
+);
+
+export default hot(module)(App);
