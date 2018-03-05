2600214a48ec5af9e4f14d0e9cf1c10ee8be18c0
Summit <JavaDog.Dev@gmail.com>
2018-03-05 03:04:20 -0500

    Flesh out the trough

---
+++ webclient/src/Trough/TroughTwat.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import './TroughTwat.scss';
+
+const TroughTwat = ({ twat }) => (
+  <div className="trough-twat">
+    <div className="user-icon">
+      <i className="material-icons">face</i>
+    </div>
+
+    <div className="twat-content">
+      <div className="heading">
+        <span className="display-name">{twat.user.displayName}</span>
+        <span>{twat.user.username}</span>
+        &nbsp; &middot; &nbsp;
+        <span>2h</span>
+        <div className="menu-button">
+          <i className="material-icons">expand_more</i>
+        </div>
+      </div>
+      <p className="twat-text">{twat.twatText}</p>
+      <div className="options-bar">
+        <i className="material-icons">reply</i>
+        <i className="material-icons">autorenew</i>
+        <i className="material-icons">favorite_border</i>
+        <i className="material-icons">message</i>
+      </div>
+    </div>
+  </div>
+);
+
+export default TroughTwat;
