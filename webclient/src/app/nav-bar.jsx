351e80242e070bbd71d1a50ef240e70eef4d89e0
Summit <JavaDog.Dev@gmail.com>
2018-03-06 14:17:25 -0500

    Added settings button and search to navbar

--- webclient/src/app/nav-bar.jsx
+++ webclient/src/app/nav-bar.jsx
@@ -11,6 +11,8 @@ const NavBar = () => (
       <NavButton iconName="notifications" buttonText="Notifications" />
       <NavButton iconName="email" buttonText="Messages" />
 
+      <SearchBox />
+      <SettingsMenu />
       <TwatButton />
     </div>
   </header>
@@ -19,6 +21,18 @@ const NavBar = () => (
 const NavButton = ({ iconName, buttonText, active }) => (
   <div className={classNames('nav-button', { active })}>
     <i className="material-icons">{iconName}</i> {buttonText}
+  </div>
+);
+
+const SearchBox = () => (
+  <div className="search-box">
+    <input type="text" placeholder="Search..." />
+  </div>
+);
+
+const SettingsMenu = () => (
+  <div className="settings-menu">
+    <i className="material-icons">face</i>
   </div>
 );
 
