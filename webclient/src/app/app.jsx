f8442f8497746f4ccad5e3230fe2943a6e9b2439
Summit <JavaDog.Dev@gmail.com>
2018-03-10 11:47:52 -0500

    Add trends box

--- webclient/src/app/app.jsx
+++ webclient/src/app/app.jsx
@@ -44,8 +44,30 @@ const App = () => (
 
     <main>
       <div className="user-info">&#47;&#47; User info and such</div>
+
       <Trough twats={devTwats} />
-      <div className="trends">&#47;&#47; trends</div>
+
+      <div className="trends">
+        <h3>Trends for you</h3>
+        <ul>
+          <li>
+            <div className="trend">Trend Thing Here 1</div>
+            <div className="twat-quantity">6543 Twats</div>
+          </li>
+          <li>
+            <div className="trend">Trend Thing Here 2</div>
+            <div className="twat-quantity">7654 Twats</div>
+          </li>
+          <li>
+            <div className="trend">Trend Thing Here 3</div>
+            <div className="twat-quantity">876556 Twats</div>
+          </li>
+          <li>
+            <div className="trend">Trend Thing Here 4</div>
+            <div className="twat-quantity">765454 Twats</div>
+          </li>
+        </ul>
+      </div>
     </main>
   </div>
 );
