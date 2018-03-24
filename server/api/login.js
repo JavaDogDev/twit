c2bff1a4ae7e09135bd66550646130990512c791
Summit <JavaDog.Dev@gmail.com>
2018-03-24 09:47:29 -0400

    Add /api/login endpoint

---
+++ server/api/login.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const User = require('../database/user');
+
+const loginRouter = express.Router();
+loginRouter.use(bodyParser.json());
+
+loginRouter.post('/', (req, res) => {
+  const { username, password } = req.body;
+
+  // check if user exists
+  User.findOne({ username }).exec((userError, user) => {
+    if (userError) {
+      console.log(`Error querying users: ${userError}`);
+      return res.redirect('/login');
+    }
+
+    if (!user) {
+      console.log(`Error finding user with username: ${username}`);
+      return res.redirect('/login');
+    }
+
+    // found them, check password
+    user.comparePassword(password, (passError, isMatch) => {
+      if (passError) {
+        console.log(`Error while checking password: ${passError}`);
+        return res.redirect('/login');
+      }
+
+      if (isMatch) {
+        console.log(`User successfully logged in: ${username}`);
+        return res.redirect('/');
+      }
+
+      console.log(`Incorrect password for user: ${username}`);
+      return res.redirect('/login');
+    });
+  });
+});
+
+module.exports = loginRouter;
