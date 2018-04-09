f19e28172fa734726e96fdc7b054122aef2bb676
Summit <JavaDog.Dev@gmail.com>
2018-04-09 18:38:16 -0400

    Wire up TwatComposerInline
    also extract it into its own file, rather than having it be
    part of Trough's file.

--- webclient/src/trough/trough.jsx
+++ webclient/src/trough/trough.jsx
@@ -3,14 +3,13 @@ import axios from 'axios';
 
 import ListTwat from '../misc/list-twat';
 import TroughLoadingSpinner from './trough-loading-spinner';
+import TwatComposerInline from './twat-composer-inline';
 import './trough.scss';
 
 class Trough extends React.Component {
   constructor() {
     super();
-    this.state = { showSpinner: true, twats: [], composerText: '' };
-    this.handleComposerInput = this.handleComposerInput.bind(this);
-    this.submitNewTwat = this.submitNewTwat.bind(this);
+    this.state = { showSpinner: true, twats: [] };
     this.refreshTrough = this.refreshTrough.bind(this);
   }
 
@@ -23,54 +22,19 @@ class Trough extends React.Component {
       .then(res => this.setState({
         showSpinner: false,
         twats: res.data.twats.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp)),
-        composerText: '',
       }))
       .catch(err => console.log(`Error getting Twats from followed users: ${err}`));
-  }
-
-  submitNewTwat() {
-    const twatText = this.state.composerText;
-    axios.post('/api/twats', { twatText })
-      .then(this.refreshTrough)
-      .catch(err => console.error(`Error submitting new Twat: ${err}`));
-  }
-
-  handleComposerInput(event) {
-    this.setState({ composerText: event.target.value });
   }
 
   render() {
     return (
       <div className="trough">
-        <TwatComposerInline
-          submitNewTwat={this.submitNewTwat}
-          handleComposerInput={this.handleComposerInput}
-        />
+        <TwatComposerInline refreshTrough={this.refreshTrough} />
         {this.state.showSpinner ? <TroughLoadingSpinner /> : null}
         {this.state.twats.map(twat => <ListTwat twat={twat} key={twat._id} />)}
       </div>
     );
   }
 }
-
-const TwatComposerInline = ({ submitNewTwat, handleComposerInput }) => (
-  <div className="twat-composer-inline">
-    <div className="user-icon">
-      <i className="material-icons">face</i>
-    </div>
-    <input
-      type="text"
-      placeholder="Don't think, just type."
-      onChange={handleComposerInput}
-    />
-    <button
-      className="twat-button"
-      tabIndex={0}
-      onClick={submitNewTwat}
-    >
-      Twat
-    </button>
-  </div>
-);
 
 export default Trough;
---
+++ webclient/src/trough/twat-composer-inline.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import axios from 'axios';
+
+import './twat-composer-inline.scss';
+
+class TwatComposerInline extends React.Component {
+  constructor() {
+    super();
+    this.state = { composerText: '' };
+    this.handleComposerInput = this.handleComposerInput.bind(this);
+    this.submitNewTwat = this.submitNewTwat.bind(this);
+  }
+
+  handleComposerInput(event) {
+    this.setState({ composerText: event.target.value });
+  }
+
+  submitNewTwat() {
+    const twatText = this.state.composerText;
+    axios.post('/api/twats', { twatText })
+      .then(() => { this.setState({ composerText: '' }); })
+      .then(this.props.refreshTrough)
+      .catch(err => console.error(`Error submitting new Twat: ${err}`));
+  }
+
+  render() {
+    return (
+      <div className="twat-composer-inline">
+        <div className="user-icon">
+          <i className="material-icons">face</i>
+        </div>
+        <input
+          type="text"
+          value={this.state.composerText}
+          placeholder="Don't think, just type."
+          onChange={this.handleComposerInput}
+        />
+        <button
+          className="twat-button"
+          tabIndex={0}
+          onClick={this.submitNewTwat}
+        >
+          Twat
+        </button>
+      </div>
+    );
+  }
+}
+
+export default TwatComposerInline;
