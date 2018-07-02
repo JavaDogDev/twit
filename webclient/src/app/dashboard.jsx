0c506e0886f76ddfffc5c3e76e9bae00ffe3d896
Summit <JavaDog.Dev@gmail.com>
2018-07-01 21:49:41 -0400

    Move routing logic to App component
    Now Dashboard and UserPage won't have to render NavBar or modal dialogs
    independently.

--- webclient/src/app/dashboard.jsx
+++ webclient/src/app/dashboard.jsx
@@ -1,101 +1,60 @@
 import * as React from 'react';
-import { ModalContainer, ModalRoute } from 'react-router-modal';
-import 'react-router-modal/css/react-router-modal.css';
 
-import NavBar from './nav-bar';
 import Trough from '../trough/trough';
-import ModalTwatComposer from '../misc/modal-twat-composer';
-import ModalTwat from '../misc/modal-twat';
 import './dashboard.scss';
 
-class Dashboard extends React.Component {
-  constructor() {
-    super();
-    this.state = { twatComposerOpen: false };
-    this.hideModalTwatComposer = this.hideModalTwatComposer.bind(this);
-    this.showModalTwatComposer = this.showModalTwatComposer.bind(this);
-  }
+const Dashboard = () => (
+  <div className="dashboard-wrapper">
+    <main>
+      <div className="user-info">
+        <div className="header-image" />
 
-  hideModalTwatComposer() {
-    this.setState({ twatComposerOpen: false });
-  }
-
-  showModalTwatComposer() {
-    this.setState({ twatComposerOpen: true });
-  }
-
-  render() {
-    return (
-      <div className="dashboard-wrapper">
-        <NavBar showModalTwatComposer={this.showModalTwatComposer} />
-
-        <main>
-          <div className="user-info">
-            <div className="header-image" />
-
-            <div className="user-info-contents">
-              <div>
-                <i className="material-icons">face</i>
-                <div>
-                  <div className="display-name">Display Name</div>
-                  <div className="username">@username</div>
-                </div>
-              </div>
-              <div className="user-stats">
-                <div className="user-stats-column">
-                  <div>Twats</div>
-                  <div>45</div>
-                </div>
-                <div className="user-stats-column">
-                  <div>Following</div>
-                  <div>954</div>
-                </div>
-              </div>
+        <div className="user-info-contents">
+          <div>
+            <i className="material-icons">face</i>
+            <div>
+              <div className="display-name">Display Name</div>
+              <div className="username">@username</div>
             </div>
           </div>
-
-          <Trough />
-
-          <div className="trends">
-            <h3>Trends for you</h3>
-            <ul>
-              <li>
-                <div className="trend">Trend Thing Here 1</div>
-                <div className="twat-quantity">6543 Twats</div>
-              </li>
-              <li>
-                <div className="trend">Trend Thing Here 2</div>
-                <div className="twat-quantity">7654 Twats</div>
-              </li>
-              <li>
-                <div className="trend">Trend Thing Here 3</div>
-                <div className="twat-quantity">876556 Twats</div>
-              </li>
-              <li>
-                <div className="trend">Trend Thing Here 4</div>
-                <div className="twat-quantity">765454 Twats</div>
-              </li>
-            </ul>
+          <div className="user-stats">
+            <div className="user-stats-column">
+              <div>Twats</div>
+              <div>45</div>
+            </div>
+            <div className="user-stats-column">
+              <div>Following</div>
+              <div>954</div>
+            </div>
           </div>
-
-          <ModalTwatComposer
-            visible={this.state.twatComposerOpen}
-            hideModalTwatComposer={this.hideModalTwatComposer}
-          />
-
-          <ModalRoute
-            exact
-            path="/twat/:twatId"
-            parentPath={this.props.match.url}
-            className="react-router-modal__modal modal-twat"
-            component={ModalTwat}
-          />
-        </main>
-
-        <ModalContainer />
+        </div>
       </div>
-    );
-  }
-}
+
+      <Trough />
+
+      <div className="trends">
+        <h3>Trends for you</h3>
+        <ul>
+          <li>
+            <div className="trend">Trend Thing Here 1</div>
+            <div className="twat-quantity">6543 Twats</div>
+          </li>
+          <li>
+            <div className="trend">Trend Thing Here 2</div>
+            <div className="twat-quantity">7654 Twats</div>
+          </li>
+          <li>
+            <div className="trend">Trend Thing Here 3</div>
+            <div className="twat-quantity">876556 Twats</div>
+          </li>
+          <li>
+            <div className="trend">Trend Thing Here 4</div>
+            <div className="twat-quantity">765454 Twats</div>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </div>
+);
 
 export default Dashboard;
