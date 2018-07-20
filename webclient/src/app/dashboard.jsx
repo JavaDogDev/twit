cd1cea7ebc74f5fb979755309268e32467d8f377
Summit <JavaDog.Dev@gmail.com>
2018-07-19 21:20:03 -0400

    Convert Trough into a dumb view
    Makes it more reusable. Now its parent component is responsible
    for giving it state like isLoading and the Twats it'll display.
    
    Also take care of mapping Redux state to props in the Dashboard
    component, since Trough will be used for other things and
    shouldn't be dealing with state that's only relevant to Dashboard.

--- webclient/src/app/dashboard.jsx
+++ webclient/src/app/dashboard.jsx
@@ -1,60 +1,82 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 
+import { refreshDashboardTroughAsync } from '../action-creators/dashboard-actions';
 import Trough from '../trough/trough';
 import './dashboard.scss';
 
-const Dashboard = () => (
-  <div className="dashboard-wrapper">
-    <main>
-      <div className="user-info">
-        <div className="header-image" />
+function mapStateToProps(state) {
+  return { twats: state.dashboard.twats };
+}
 
-        <div className="user-info-contents">
-          <div>
-            <i className="material-icons">face</i>
-            <div>
-              <div className="display-name">Display Name</div>
-              <div className="username">@username</div>
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      troughLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.props.dispatch(refreshDashboardTroughAsync())
+      .then(() => this.setState({ troughLoading: false }));
+  }
+
+  render() {
+    return (
+      <div className="dashboard-wrapper">
+        <main>
+          <div className="user-info">
+            <div className="header-image" />
+
+            <div className="user-info-contents">
+              <div>
+                <i className="material-icons">face</i>
+                <div>
+                  <div className="display-name">Display Name</div>
+                  <div className="username">@username</div>
+                </div>
+              </div>
+              <div className="user-stats">
+                <div className="user-stats-column">
+                  <div>Twats</div>
+                  <div>45</div>
+                </div>
+                <div className="user-stats-column">
+                  <div>Following</div>
+                  <div>954</div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="user-stats">
-            <div className="user-stats-column">
-              <div>Twats</div>
-              <div>45</div>
-            </div>
-            <div className="user-stats-column">
-              <div>Following</div>
-              <div>954</div>
-            </div>
+
+          <Trough twats={this.props.twats} isLoading={this.state.troughLoading} showTwatComposer />
+
+          <div className="trends">
+            <h3>Trends for you</h3>
+            <ul>
+              <li>
+                <div className="trend">Trend Thing Here 1</div>
+                <div className="twat-quantity">6543 Twats</div>
+              </li>
+              <li>
+                <div className="trend">Trend Thing Here 2</div>
+                <div className="twat-quantity">7654 Twats</div>
+              </li>
+              <li>
+                <div className="trend">Trend Thing Here 3</div>
+                <div className="twat-quantity">876556 Twats</div>
+              </li>
+              <li>
+                <div className="trend">Trend Thing Here 4</div>
+                <div className="twat-quantity">765454 Twats</div>
+              </li>
+            </ul>
           </div>
-        </div>
+        </main>
       </div>
+    );
+  }
+}
 
-      <Trough />
-
-      <div className="trends">
-        <h3>Trends for you</h3>
-        <ul>
-          <li>
-            <div className="trend">Trend Thing Here 1</div>
-            <div className="twat-quantity">6543 Twats</div>
-          </li>
-          <li>
-            <div className="trend">Trend Thing Here 2</div>
-            <div className="twat-quantity">7654 Twats</div>
-          </li>
-          <li>
-            <div className="trend">Trend Thing Here 3</div>
-            <div className="twat-quantity">876556 Twats</div>
-          </li>
-          <li>
-            <div className="trend">Trend Thing Here 4</div>
-            <div className="twat-quantity">765454 Twats</div>
-          </li>
-        </ul>
-      </div>
-    </main>
-  </div>
-);
-
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard);
--- webclient/src/trough/trough.jsx
+++ webclient/src/trough/trough.jsx
@@ -1,36 +1,16 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
 
-import { refreshTroughAsync } from '../action-creators/trough-actions';
 import ListTwat from '../misc/list-twat';
 import InlineLoadingSpinner from '../misc/inline-loading-spinner';
 import TwatComposerInline from './twat-composer-inline';
 import './trough.scss';
 
-function mapStateToProps(state) {
-  return { twats: state.trough.twats };
-}
+const Trough = ({ twats, isLoading, showTwatComposer }) => (
+  <div className="trough">
+    {showTwatComposer ? <TwatComposerInline /> : null}
+    {isLoading ? <InlineLoadingSpinner /> : null}
+    {twats.map(twat => <ListTwat twat={twat} key={twat._id} />)}
+  </div>
+);
 
-class Trough extends React.Component {
-  constructor() {
-    super();
-    this.state = { showSpinner: true };
-  }
-
-  componentDidMount() {
-    this.props.dispatch(refreshTroughAsync())
-      .then(() => this.setState({ showSpinner: false }));
-  }
-
-  render() {
-    return (
-      <div className="trough">
-        <TwatComposerInline />
-        {this.state.showSpinner ? <InlineLoadingSpinner /> : null}
-        {this.props.twats.map(twat => <ListTwat twat={twat} key={twat._id} />)}
-      </div>
-    );
-  }
-}
-
-export default connect(mapStateToProps)(Trough);
+export default Trough;
